e7853e9c15f24053ff1b88a0a76564b39efba019
Will Frey <jfrey89@gmail.com>
2013-05-05 20:11:15 -0400

    :O

--- scripts/stokes.py
+++ scripts/stokes.py
@@ -19,7 +19,40 @@ def get_coordinates(element, nodes):
         coordinates[index] = nodes[vertex - 1]
 
     return coordinates
+    
 
+def quadratic_basis(index, x, y):
+    if index == 0:
+        coefficients = np.array([1, -3, -3, 4, 2, 2])
+    elif index == 1:
+        coefficients = np.array([0, -1, 0, 0, 2, 0])
+    elif index == 2:
+        coefficients = np.array([0, 0, -1, 0, 0, 2])
+    elif index == 3:
+        coefficients = np.array([0, 4, 0, -4, -4, 0])
+    elif index == 4:
+        coefficients = np.array([0, 0, 4, -4, 0, -4])
+    elif index == 5:
+        coefficients = np.array([0, 0, 0, 4, 0, 0])
+        
+    return np.dot(np.array([1, x, y, x * y, x * x, y * y]), coefficients)
+
+
+def quadratic_basis_gradient(index, x, y):
+    if index == 0:
+        gradient_coefficients = np.array([[-3, -3], [4, 4], [4, 4]])
+    elif index == 1:
+        gradient_coefficients = np.array([[-1, 0], [4, 0], [4, 4]])
+    elif index == 2:
+        gradient_coefficients = np.array([[0, -1], [0, 0], [0, 4]])
+    elif index == 3:
+        gradient_coefficients = np.array([[4, 0], [-8, 4], [4, 0]])
+    elif index == 4:
+        gradient_coefficients = np.array([[0, 4], [0, -4], [-4, -8]])
+    elif index == 5:
+        gradient_coefficients = np.array([[0, 0], [0, 4], [4, 0]])
+        
+    return np.dot(np.array([1, x, y]), gradient_coefficients)
 
 def calculate_jacobian(coordinates):
     # unpack coordinates
@@ -41,13 +74,15 @@ if __name__ == '__main__':
     A = np.zeros((len(nodes), len(nodes)))
     B = np.zeros((len(nodes), len(nodes)))
     C = np.zeros((len(nodes), len(nodes)))
-
+    
+    
     for element in elements:
         coordinates = get_coordinates(element, nodes)
         corners = coordinates[:4]
         J = calculate_jacobian(corners)
-
+        
         for i in xrange(6):
             for j in xrange(i, 6):
-                A(i, j) = A(i, j) +
-
+                
+                pass
+            pass
