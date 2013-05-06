fd945e7761de180d5281a0291abad511241718da
Will Frey <jfrey89@gmail.com>
2013-05-06 13:10:02 -0400

    new file:   files/unit-square_h-0.5.geo 	new file:   files/unit-square_h-0.5.mesh 	new file:   files/unit_square-pressure.mesh 	new file:   files/unit_square-velocity.mesh 	modified:   scripts/stokes.py 	new file:   sfepy

--- scripts/stokes.py
+++ scripts/stokes.py
@@ -20,40 +20,29 @@ def get_coordinates(element, nodes):
         coordinates[index] = nodes[vertex - 1]
 
     return coordinates
-    
-
-def quadratic_basis(index, x, y):
-    if index == 0:
-        coefficients = np.array([1, -3, -3, 4, 2, 2])
-    elif index == 1:
-        coefficients = np.array([0, -1, 0, 0, 2, 0])
-    elif index == 2:
-        coefficients = np.array([0, 0, -1, 0, 0, 2])
-    elif index == 3:
-        coefficients = np.array([0, 4, 0, -4, -4, 0])
-    elif index == 4:
-        coefficients = np.array([0, 0, 4, -4, 0, -4])
-    elif index == 5:
-        coefficients = np.array([0, 0, 0, 4, 0, 0])
-        
-    return np.dot(np.array([1, x, y, x * y, x * x, y * y]), coefficients)
 
 
-def quadratic_basis_gradient(index, x, y):
-    if index == 0:
-        gradient_coefficients = np.array([[-3, -3], [4, 4], [4, 4]])
-    elif index == 1:
-        gradient_coefficients = np.array([[-1, 0], [4, 0], [4, 4]])
-    elif index == 2:
-        gradient_coefficients = np.array([[0, -1], [0, 0], [0, 4]])
-    elif index == 3:
-        gradient_coefficients = np.array([[4, 0], [-8, 4], [4, 0]])
-    elif index == 4:
-        gradient_coefficients = np.array([[0, 4], [0, -4], [-4, -8]])
-    elif index == 5:
-        gradient_coefficients = np.array([[0, 0], [0, 4], [4, 0]])
-        
-    return np.dot(np.array([1, x, y]), gradient_coefficients)
+def quad_basis(index, x, y):
+    coefficients = np.array([[1, -3, -3, 4, 2, 2],
+                             [0, -1, 0, 0, 2, 0],
+                             [0, 0, -1, 0, 0, 2],
+                             [0, 4, 0, -4, -4, 0],
+                             [0, 0, 4, -4, 0, -4],
+                             [0, 0, 0, 4, 0, 0]])
+    return np.dot(coefficients[index],
+                  np.array([1, x, y, x * y, x * x, y * y]))
+
+
+def quad_basis_gradient(index, x, y):
+    coefficients = np.array([[-3, 4, 4], [-3, 4, 4],
+                             [-1, 4, 0], [0, 0, 0],
+                             [0, 0, 0], [-1, 0, 4],
+                             [4, -8, -4], [0, -4, 0],
+                             [0, 0, -4], [4, -4, -8],
+                             [0, 0, 4], [0, 4, 0]])
+
+    return np.dot(coefficients[index:index + 2], np.array([1, x, y]))
+
 
 def calculate_jacobian(coordinates):
     # unpack coordinates
@@ -64,9 +53,10 @@ def calculate_jacobian(coordinates):
     return np.linalg.inv(np.array([[x1 - x3, x2 - x3],
                                    [y1 - y3, y2 - y3]]))
 
+
 if __name__ == '__main__':
     root_dir = './files/'
-    mesh_file = 'unit-square_h-0.3.mesh'
+    mesh_file = 'unit-square_h-0.5.mesh'
     # load mesh
     domain = m.mesh_factory(root_dir + mesh_file)
     elements = domain.elements
@@ -75,40 +65,41 @@ if __name__ == '__main__':
     stiffness_velocity_x = np.zeros((len(nodes), len(nodes)))
     stiffness_velocity_y = np.zeros((len(nodes), len(nodes)))
     stiffness_pressure = np.zeros((len(nodes), len(nodes)))
-    
-    
+
     for element in elements:
-        
+
         coordinates = get_coordinates(element, nodes)
         corners = coordinates[:3]
         J = calculate_jacobian(corners)
-        
+
         for i in xrange(6):
             for j in xrange(i, 6):
-                
-                quad_grad_basis = lambda k, x, y: \
-                np.dot(J.T, quadratic_basis_gradient(k, x, y))
-                
-                dot_product = lambda x, y: \
-                np.dot(quad_grad_basis(i, x, y), quad_grad_basis(j, x, y)) * \
-                np.linalg.det(J)
-                
-                I_x = (dot_product(0.5, 0.5) + dot_product(0.0, 0.5) + \
-                dot_product(0.5, 0.0)) / 6
-                
-                I_y = (dot_product(0.5, 0.5) + dot_product(0.0, 0.5) + \
-                dot_product(0.5, 0.0)) / 6
-                
+
+                grad_basis = lambda k, x, y: \
+                    np.dot(J.T, quad_basis_gradient(k, x, y))
+
+                grad_dot_grad = lambda x, y: \
+                    np.dot(grad_basis(i, x, y), grad_basis(j, x, y)) / \
+                    np.abs(np.linalg.det(J))
+
+                I_x = (grad_dot_grad(0.5, 0.5) + grad_dot_grad(0.0, 0.5) +
+                       grad_dot_grad(0.5, 0.0)) / 6
+
+                I_y = (grad_dot_grad(0.5, 0.5) + grad_dot_grad(0.0, 0.5) +
+                       grad_dot_grad(0.5, 0.0)) / 6
+
+                # scatter the local operator back
                 stiffness_velocity_x[element[i] - 1, element[j] - 1] = \
-                stiffness_velocity_x[element[i] - 1, element[j] - 1] + I_x
-                
+                    stiffness_velocity_x[element[i] - 1, element[j] - 1] + I_x
+
                 stiffness_velocity_y[element[i] - 1, element[j] - 1] = \
-                stiffness_velocity_y[element[i] - 1, element[j] - 1] + I_y
-                
+                    stiffness_velocity_y[element[i] - 1, element[j] - 1] + I_y
+
                 stiffness_velocity_x[element[j] - 1, element[i] - 1] = \
-                stiffness_velocity_x[element[i] - 1, element[j] - 1]
-                
+                    stiffness_velocity_x[element[i] - 1, element[j] - 1]
+
                 stiffness_velocity_y[element[j] - 1, element[i] - 1] = \
-                stiffness_velocity_y[element[i] - 1, element[j] - 1]                
+                    stiffness_velocity_y[element[i] - 1, element[j] - 1]
+
                 pass
             pass
