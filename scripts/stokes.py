463ca67b43e44d21631640a9c0ba86f92d2e8ced
Will Frey <jfrey89@gmail.com>
2013-05-06 18:55:05 -0400

    new file:   files/stiffness.txt 	modified:   scripts/stokes.py

--- scripts/stokes.py
+++ scripts/stokes.py
@@ -34,14 +34,34 @@ def quad_basis(index, x, y):
 
 
 def quad_basis_gradient(index, x, y):
-    coefficients = np.array([[-3, 4, 4], [-3, 4, 4],
-                             [-1, 4, 0], [0, 0, 0],
-                             [0, 0, 0], [-1, 0, 4],
-                             [4, -8, -4], [0, -4, 0],
-                             [0, 0, -4], [4, -4, -8],
-                             [0, 0, 4], [0, 4, 0]])
+    coefficients = np.array([[[-3, 4, 4], [-3, 4, 4]],
+                             [[-1, 4, 0], [0, 0, 0]],
+                             [[0, 0, 0], [-1, 0, 4]],
+                             [[4, -8, -4], [0, -4, 0]],
+                             [[0, 0, -4], [4, -4, -8]],
+                             [[0, 0, 4], [0, 4, 0]]])
 
-    return np.dot(coefficients[index:index + 2], np.array([1, x, y]))
+    return np.dot(coefficients[index], np.array([1, x, y]))
+
+
+def lin_basis(index, x, y):
+    coefficients = np.array([[1, -1, -1],
+                             [0, 1, 0],
+                             [0, 0, 1]])
+
+    return np.dot(coefficients[index], np.array([1, x, y]))
+    pass
+
+
+def lin_basis_deriv(index, dvar):
+    coefficients = np.array([[-1, -1],
+                             [1, 0],
+                             [0, 1]])
+
+    if dvar == 0 or dvar == 'x':
+        return coefficients[index, 0]
+    else:
+        return coefficients[index, 1]
 
 
 def calculate_jacobian(coordinates):
@@ -62,44 +82,64 @@ if __name__ == '__main__':
     elements = domain.elements
     nodes = domain.nodes
 
-    stiffness_velocity_x = np.zeros((len(nodes), len(nodes)))
-    stiffness_velocity_y = np.zeros((len(nodes), len(nodes)))
-    stiffness_pressure = np.zeros((len(nodes), len(nodes)))
+    stiff_vx = np.zeros((len(nodes), len(nodes)))
+    stiff_vy = np.zeros((len(nodes), len(nodes)))
+    stiff_p = np.zeros((len(nodes), len(nodes)))
 
     for element in elements:
 
         coordinates = get_coordinates(element, nodes)
         corners = coordinates[:3]
         J = calculate_jacobian(corners)
+        detJ = np.abs(np.linalg.det(J))
 
-        for i in xrange(6):
-            for j in xrange(i, 6):
+        for i, node_i in enumerate(element):
+            for j, node_j in enumerate(element[i:]):
+
+                node_i = node_i - 1
+                node_j = node_j - 1
 
                 grad_basis = lambda k, x, y: \
                     np.dot(J.T, quad_basis_gradient(k, x, y))
 
+                if j < 3:
+                    pres_cont_x = lambda x, y: \
+                        grad_basis(i, x, y)[0] * lin_basis(j, x, y) / detJ
+
+                    stiff_vx[node_i, node_j] += \
+                        (pres_cont_x(0.5, 0.5) + pres_cont_x(0.0, 0.5) +
+                         pres_cont_x(0.5, 0.0)) / (6 * detJ)
+
+                    pres_cont_y = lambda x, y: \
+                        grad_basis(i, x, y)[1] * lin_basis(j, x, y) / detJ
+
+                    stiff_vy[node_i, node_j] += \
+                        (pres_cont_y(0.5, 0.5) + pres_cont_y(0.0, 0.5) +
+                         pres_cont_y(0.5, 0.0)) / (6 * detJ)
+
                 grad_dot_grad = lambda x, y: \
-                    np.dot(grad_basis(i, x, y), grad_basis(j, x, y)) / \
-                    np.abs(np.linalg.det(J))
+                    np.dot(grad_basis(i, x, y), grad_basis(j, x, y)) / detJ
 
-                I_x = (grad_dot_grad(0.5, 0.5) + grad_dot_grad(0.0, 0.5) +
-                       grad_dot_grad(0.5, 0.0)) / 6
+                I_vx = (grad_dot_grad(0.5, 0.5) + grad_dot_grad(0.0, 0.5) +
+                        grad_dot_grad(0.5, 0.0)) / 6
 
-                I_y = (grad_dot_grad(0.5, 0.5) + grad_dot_grad(0.0, 0.5) +
-                       grad_dot_grad(0.5, 0.0)) / 6
+                I_vy = (grad_dot_grad(0.5, 0.5) + grad_dot_grad(0.0, 0.5) +
+                        grad_dot_grad(0.5, 0.0)) / 6
 
                 # scatter the local operator back
-                stiffness_velocity_x[element[i] - 1, element[j] - 1] = \
-                    stiffness_velocity_x[element[i] - 1, element[j] - 1] + I_x
+                stiff_vx[node_i, node_j] += I_vx
+                    #stiffness_velocity_x[element[i] - 1, element[j] - 1] + \
+                    #I_vx
 
-                stiffness_velocity_y[element[i] - 1, element[j] - 1] = \
-                    stiffness_velocity_y[element[i] - 1, element[j] - 1] + I_y
+                stiff_vy[node_i, node_j] += I_vy
+                    #stiffness_velocity_y[element[i] - 1, element[j] - 1] + \
+                    #I_vy
 
-                stiffness_velocity_x[element[j] - 1, element[i] - 1] = \
-                    stiffness_velocity_x[element[i] - 1, element[j] - 1]
+                stiff_vx[node_j, node_i] = \
+                    stiff_vx[node_i, node_j]
 
-                stiffness_velocity_y[element[j] - 1, element[i] - 1] = \
-                    stiffness_velocity_y[element[i] - 1, element[j] - 1]
+                stiff_vy[node_j, node_i] = \
+                    stiff_vy[node_i, node_j]
 
                 pass
             pass
