385f27e9786ee89938a75cdcc115c37b2bed1d50
Will Frey <jfrey89@gmail.com>
2013-05-09 20:59:26 -0400

    modified:   files/A.txt 	modified:   files/Gx.txt 	modified:   files/Gy.txt 	modified:   files/Hx.txt 	modified:   files/Hy.txt 	modified:   files/S.txt 	new file:   files/unit-square.geo 	renamed:    files/unit-square_h-0.4.mesh -> files/unit-square.mesh 	deleted:    files/unit-square_h-0.1.geo 	deleted:    files/unit-square_h-0.1.mesh 	deleted:    files/unit-square_h-0.25.geo 	deleted:    files/unit-square_h-0.25.mesh 	deleted:    files/unit-square_h-0.3.geo 	deleted:    files/unit-square_h-0.3.mesh 	deleted:    files/unit-square_h-0.33.geo 	deleted:    files/unit-square_h-0.33.mesh 	deleted:    files/unit-square_h-0.4.geo 	deleted:    files/unit-square_h-0.5.geo 	deleted:    files/unit-square_h-0.5.mesh 	deleted:    files/unit-square_h-0.7.geo 	deleted:    files/unit_square-pressure.mesh 	deleted:    files/unit_square-velocity.mesh 	modified:   scripts/.stokes.py.swp 	modified:   scripts/stokes.py

--- scripts/stokes.py
+++ scripts/stokes.py
@@ -37,13 +37,17 @@ def calculate_B(coordinates):
 if __name__ == '__main__':
     eps = sys.float_info.epsilon
     root_dir = './files/'
-    mesh_file = 'unit-square_h-0.5.mesh'
+    mesh_file = 'unit-square.mesh'
     # load mesh
     domain = m.mesh_factory(root_dir + mesh_file)
     elements = domain.elements
-    nodes_2_coords = domain.nodes
     nodes = np.unique(elements)
-    pnodes = np.unique(elements.T[:][:3])
+    pnodes = np.unique(elements.T[:3])
+    interior_nodes = domain.interior_nodes
+
+    nodes_2_coords = domain.nodes
+
+    n, m = len(nodes), len(pnodes)
 
     reynolds = 1.0
     weight = 1 / 6.0
@@ -51,10 +55,10 @@ if __name__ == '__main__':
                           [0.0, 0.5],
                           [0.5, 0.5]])
 
-    S = np.zeros((nodes.max(), nodes.max()))
-    Gx = np.zeros((nodes.max(), nodes.max()))
+    S = np.zeros((n, n))
+    Gx = np.zeros((n, m))
+    Hx = np.zeros((m, n))
     Gy = Gx.copy()
-    Hx = np.zeros((nodes.max(), nodes.max()))
     Hy = Hx.copy()
 
     f = np.zeros(3 * len(S))
@@ -64,79 +68,101 @@ if __name__ == '__main__':
     quad_basis = fn.Quadratic_Basis_Function()
     lin_basis = fn.Linear_Basis_Function()
 
-    for element in elements:
+    for count, element in enumerate(elements):
+        print '*' * 20
+        print "Element %d of %d" % (count + 1, len(elements))
+        print '#' * 20
+
         coordinates = get_coordinates(element, nodes_2_coords)
         B = calculate_B(coordinates[:3])
         detJ = 1 / np.abs(np.linalg.det(B))
         weight_scaled = weight * detJ
 
-        for i in xrange(6):
-            for j in xrange(6):
-                node_i, node_j = element[i] - 1, element[j] - 1
+        print "Triangular element has vertices:"
+        print coordinates[:3]
 
-                # (grad u_h, grad v)
-                for point in gauss_pts:
-                    x_g, y_g = point
-
-                    S[node_i, node_j] += (weight_scaled / reynolds) * np.dot(
-                        np.dot(quad_basis.grad(i, x_g, y_g), B),
-                        np.dot(B.T, quad_basis.grad(j, x_g, y_g)))
-
-                    f2[node_j] += -weight_scaled * quad_basis(j, x_g, y_g)
-
-        # (p_h, div v)
-        for i in xrange(3):
-            for j in xrange(6):
-                node_i, node_j = element[i] - 1, element[j] - 1
-
-                for point in gauss_pts:
-                    x_g, y_g = point
-                    Gx[node_i, node_j] += weight_scaled * (
-                        lin_basis(i, x_g, y_g) *
-                        np.dot(quad_basis.grad(j, x_g, y_g), B[0]))
-
-                    Gy[node_i, node_j] += weight_scaled * (
-                        lin_basis(i, x_g, y_g) *
-                        np.dot(quad_basis.grad(j, x_g, y_g), B[1]))
+        local_S = np.zeros((6, 6))
+        local_Hx = np.zeros((3, 6))
+        local_Hy = local_Hx.copy()
+        local_Gx = np.zeros((6, 3))
+        local_Gy = local_Gx.copy()
 
         for i in xrange(6):
-            for j in xrange(3):
-                node_i, node_j = element[i] - 1, element[j] - 1
+            for j in xrange(6):
 
+                # S(i, j) = (grad psi_j, grad psi_i)
                 for point in gauss_pts:
                     x_g, y_g = point
-                    Hx[node_i, node_j] += weight_scaled * (
-                        np.dot(quad_basis.grad(i, x_g, y_g), B[0]) *
-                        lin_basis(j, x_g, y_g))
+                    local_S[i, j] += (weight_scaled / reynolds) * np.dot(
+                        np.dot(quad_basis.grad(j, x_g, y_g), B),
+                        np.dot(B.T, quad_basis.grad(i, x_g, y_g)))
 
-                    Hy[node_i, node_j] += weight_scaled * (
-                        np.dot(quad_basis.grad(i, x_g, y_g), B[1]) *
-                        lin_basis(j, x_g, y_g))
+                # H_x(i, j) = (d/dx psi_j, phi_i)
+                # H_y(i, j) = (d/dy psi_j, phi_i)
+                if i < 3:
+                    for point in gauss_pts:
+                        x_g, y_g = point
+                        local_Hx[i, j] += weight_scaled * (
+                            np.dot(quad_basis.grad(j, x_g, y_g), B[0]) *
+                            lin_basis(i, x_g, y_g))
 
-    # nuke the boundary nodes
-    boundary = domain.boundary_nodes['land']
-    for node in boundary:
-        node = node - 1
-        S[node] = np.zeros(np.shape(S[node]))
-        S.T[node] = np.zeros(np.shape(S.T[node]))
-        #Gx[node] = np.zeros(np.shape(Gx[node]))
-        Gx.T[node] = np.zeros(np.shape(Gx.T[node]))
-        #Gy[node] = np.zeros(np.shape(Gy[node]))
-        Gy.T[node] = np.zeros(np.shape(Gy.T[node]))
-        Hx[node] = np.zeros(np.shape(Hx[node]))
-        #Hx.T[node] = np.zeros(np.shape(Hx.T[node]))
-        Hy[node] = np.zeros(np.shape(Hy[node]))
-        #Hy.T[node] = np.zeros(np.shape(Hy.T[node]))
+                        local_Hy[i, j] += weight_scaled * (
+                            np.dot(quad_basis.grad(j, x_g, y_g), B[1]) *
+                            lin_basis(i, x_g, y_g))
 
-    Z = np.zeros(np.shape(S))
-    A = np.bmat([[S, Z, -Gx], [Z, S, -Gy], [Hx, Hy, Z]])
-    # make it nonsingular, nearby matrix
-    A += np.eye(np.shape(A)[0], np.shape(A)[1]) * eps
+                # G_x(i, j) = (phi_j, d/dx psi_i)
+                # G_y(i, j) = (phi_j, d/dy psi_i)
+                if j < 3:
+                    for point in gauss_pts:
+                        x_g, y_g = point
+                        local_Gx[i, j] += weight_scaled * (
+                            lin_basis(j, x_g, y_g) *
+                            np.dot(quad_basis.grad(i, x_g, y_g), B[0]))
 
-    #f[:len(f1)] += f1[:]
-    #f[len(f1):len(f1) + len(f2)] += f2[:]
+                        local_Gy[i, j] += weight_scaled * (
+                            lin_basis(j, x_g, y_g) *
+                            np.dot(quad_basis.grad(i, x_g, y_g), B[1]))
 
-    #c = spsolve(A, f)
+        # scatter the local matrices
+        for i in xrange(6):
+            for j in xrange(6):
+                # Scatter S
+                S[element[i] - 1, element[j] - 1] += local_S[i, j]
+
+                # Scatter Hx, Hy
+                # Hx, Hy is m x n
+                if i < 3:
+                    pnode_i = np.where(pnodes == element[i])[0][0]
+                    Hx[pnode_i, element[j] - 1] += local_Hx[i, j]
+                    Hy[pnode_i, element[j] - 1] += local_Hy[i, j]
+
+                # Scatter Gx, Gy
+                # Gx, Gy is n x m
+                if j < 3:
+                    pnode_j = np.where(pnodes == element[j])[0][0]
+                    Gx[element[i] - 1, pnode_j] += local_Gx[i, j]
+                    Gy[element[i] - 1, pnode_j] += local_Gy[i, j]
+
+    # Apply the boundary conditions
+    # Assuming that velocity (u, v) = (0, 0) on the boundary
+    # That implies that the mean pressure is zero over the domain
+    # The mean pressure is taken into account by allowing the space for
+    # (u, v) to not necessarily be divergence-free. It integrates to zeros
+    # against the pressure space.
+
+    S = S[interior_nodes - 1]
+    S = S[:, interior_nodes - 1]
+    Hx = Hx[:, interior_nodes - 1]
+    Hy = Hy[:, interior_nodes - 1]
+    Gx = Gx[interior_nodes - 1]
+    Gy = Gy[interior_nodes - 1]
+
+    k = len(interior_nodes)
+    A = np.bmat([[S, np.zeros((k, k)), -Gx],
+                 [np.zeros((k, k)), S, -Gy],
+                 [Hx, Hy, np.zeros((m, m))]])
+
+    A += np.eye(len(A)) * eps
 
     np.savetxt('./files/A.txt', A)
     np.savetxt('./files/S.txt', S)
@@ -144,5 +170,3 @@ if __name__ == '__main__':
     np.savetxt('./files/Gy.txt', Gy)
     np.savetxt('./files/Hx.txt', Hx)
     np.savetxt('./files/Hy.txt', Hy)
-    #np.savetxt('./files/f.txt', f)
-    #np.savetxt('./files/c.txt', c)
